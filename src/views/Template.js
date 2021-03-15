3cc3c833b454152a624f8729d38019b6dc3b22e4
Ibitoye Rotimi Best <best.rotimi@ikizmet.com>
2021-03-15 10:19:15 +0200

    remove unused var

--- src/views/Template.js
+++ src/views/Template.js
@@ -5,7 +5,6 @@ import 'bootstrap/dist/js/bootstrap.min';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
 import Container from 'react-bootstrap/Container';
 
 import {
